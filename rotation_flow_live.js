7ffa76b0dcf34164bb67325679231d20898bf182
maivi18031991 <maivi18031991@gmail.com>
2025-11-04 14:50:40 +0700

    Update rotation_flow_live.js

--- rotation_flow_live.js
+++ rotation_flow_live.js
@@ -6,7 +6,7 @@ export async function rotationFlowScan() {
   
   for (const symbol of symbols) {
     try {
-      const r = await fetch(`https://api.binance.com/api/v3/ticker/24hr?symbol=${symbol}`);
+      const r = await fetch(`https://api-gcp.binance.com/api/v3/ticker/24hr?symbol=${symbol}`);
       const d = await r.json();
       const volNow = Number(d.quoteVolume);
       const vol24 = Number(d.volume);
